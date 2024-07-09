655f9eb701ffa63098703904319cdcd341ccb806
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-07-09 15:15:23 +0200

    build: set variables and base components

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+
+    export default{
+        name: 'AppHeader',
+    }
+
+</script>
+
+<template>
+    <h1>Header</h1>
+</template>
+
+<style scoped lang="scss">
+@use '../style/general.scss' as *;
+@use '../style/partials/mixins' as *;
+@use '../style/partials/variables' as*;
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,17 @@
+<script>
+
+    export default{
+        name: 'AppMain',
+    }
+
+</script>
+
+<template>
+    <h1>Main</h1>
+</template>
+
+<style scoped lang="scss">
+@use '../style/general.scss' as *;
+@use '../style/partials/mixins' as *;
+@use '../style/partials/variables' as*;
+</style>
